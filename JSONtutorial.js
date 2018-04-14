90550b97209bdefc582d4d8616e584f32b4cf786
oconr003 <rachel-oc@hotmail.co.uk>
2018-04-14 14:15:57 +0100

    JSON practice

---
+++ JSONtutorial.js
@@ -0,0 +1,19 @@
+/** This should be a JSON file not js
+ * 
+ * {
+ * 	"people": [
+ * 		var people = [
+        {
+          "name":"Brad",
+          "age": 35
+        },
+        {
+          "name":"John",
+          "age":40
+        },
+        {
+          "name":"Sara",
+          "age":25
+        }
+      ];
+ */
